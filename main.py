63bec72de2481ea67832e10adf3b14883e35ed49
1234 <vasilyevaapolina2006@gmail.com>
2022-12-17 19:22:17 +0500

    add class cat and other

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ white = (255, 255, 255)
 black = (0, 0, 0)
 gray = (120, 120, 120)
 WIDTH = 400
-HEIGHT = 500
+HEIGHT = 600
 background = white
-player = pygame.transform.scale(pygame.image.load('cat.png'), (50, 40))
+player = pygame.transform.scale(pygame.image.load('cat.png'), (40, 40))
 player_r = pygame.transform.flip(pygame.transform.rotate(player, 90), True, False)
 player_l = pygame.transform.flip(player_r, True, False)
+dead = pygame.transform.scale(pygame.image.load('died_cat.jpg'), (40, 40))
 
 
 class Cat(pygame.sprite.Sprite):
@@ -21,7 +22,8 @@ class Cat(pygame.sprite.Sprite):
         self.image_r = player_r
         self.image_l = player_l
         self.player_x = 110
-        self.player_y = 400
+        self.player_y = 500
+        self.image_dead = dead
         self.jump = False
         self.jump_1 = False
         self.jump_height = 100
@@ -36,19 +38,22 @@ class Cat(pygame.sprite.Sprite):
 
     def current_block(self, rect_list):
         for i in range(len(rect_list)):
-            if rect_list[i].colliderect([self.player_x + 10, self.player_y, 50, 40]):
+            if rect_list[i].colliderect([self.player_x + 10, self.player_y, 40, 40]):
                 return i
-            if rect_list[i].colliderect([self.player_x - 10, self.player_y, 50, 40]):
+            if rect_list[i].colliderect([self.player_x - 10, self.player_y, 40, 40]):
                 return i
 
     def check_collisions(self, rect_list, j):
         for i in range(len(rect_list)):
-            if rect_list[i].colliderect([self.player_x, self.player_y, 50, 40]) and rect_list[self.curr_block][0] < self.player_x and i != self.curr_block:# platform left
+            if rect_list[i].colliderect([self.player_x, self.player_y, 40, 40]) and rect_list[self.curr_block][0] < self.player_x and i != self.curr_block:# platform left
                 self.curr_block = i
+
+
                 return True
 
-            if rect_list[i].colliderect([self.player_x, self.player_y, 50, 40]) and rect_list[self.curr_block][0] > self.player_x and i != self.curr_block:
+            if rect_list[i].colliderect([self.player_x, self.player_y, 40, 40]) and rect_list[self.curr_block][0] > self.player_x and i != self.curr_block:
                 self.curr_block = i
+                self.player_x = rect_list[i][0] + 10
                 return True
 
     def one_click(self, x_pos, y_pos):
@@ -61,6 +66,9 @@ class Cat(pygame.sprite.Sprite):
             self.gravity_y -= 1
             return x_pos, y_pos
 
+    def slip(self):
+        self.player_y += 0.3
+
 
 fps = 60
 flip = False
@@ -69,7 +77,7 @@ timer = pygame.time.Clock()
 
 # game variables
 
-platforms = [[100, 370, 10, 100], [200, 310, 10, 100], [100, 240, 10, 100], [200, 100, 10, 100]]
+platforms = [[100, 479, 10, 130], [200, 410, 10, 130], [100, 310, 10, 130], [200, 200, 10, 130]]
 
 v = 50
 
@@ -85,31 +93,35 @@ while running:
     for i in range(len(platforms)):
         block = pygame.draw.rect(screen, black, platforms[i], 0, 2)
         blocks.append(block)
-    #if cat.jump:
-    screen.blit(cat.image, (cat.player_x, cat.player_y))
-    '''
+    if cat.check_fall(cat.player_x, cat.player_y):
+        cat.jump = False
+        screen.blit(cat.image_dead, (cat.player_x, cat.player_y))
+    elif cat.jump:
+        screen.blit(cat.image, (cat.player_x, cat.player_y))
     else:
         curr_block = cat.current_block(blocks)
-        if blocks[curr_block][0] <= cat.player_x:
+        if blocks[curr_block][1] > cat.player_y:
+            screen.blit(cat.image_r, (cat.player_x, cat.player_y))
+        elif blocks[curr_block][0] <= cat.player_x:
             screen.blit(cat.image_r, (cat.player_x, cat.player_y))
         else:
             screen.blit(cat.image_l, (cat.player_x, cat.player_y))
-        '''
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
+            cat.gravity_y = 15
+            cat.gravity_x = 2
             cat.jump = True
             cat.jump_1 = True
 
-    if cat.check_fall(cat.player_x, cat.player_y):
-        cat.jump = False
-    if cat.jump_1 and cat.jump and not cat.check_collisions(blocks, cat.curr_block):
+
+    if cat.jump_1 and cat.jump and not cat.check_collisions(blocks, cat.curr_block) and not cat.check_fall(cat.player_x, cat.player_y):
         cat.player_x, cat.player_y = cat.one_click(cat.player_x, cat.player_y)
-        print(cat.player_x)
     if cat.check_collisions(blocks, cat.curr_block):
         cat.jump = False
+
 
     # player_x, player_y = update_player(player_x, player_y)
 
