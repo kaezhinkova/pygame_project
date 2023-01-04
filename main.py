1b9dd6c855b732ba5b49c27d1ceecc3a3bf72fe6
1234 <vasilyevaapolina2006@gmail.com>
2023-01-04 17:11:28 +0500

    зубчатые блоки

--- main.py
+++ main.py
@@ -26,6 +26,8 @@ player_l = pygame.transform.flip(player_r, True, False)
 dead = pygame.transform.scale(pygame.image.load('died_cat.jpg'), (40, 40))
 
 last_block = 0
+LEVEL = 1
+block_w_teeth = 0
 
 
 class Block(pygame.sprite.Sprite):
@@ -34,38 +36,63 @@ class Block(pygame.sprite.Sprite):
     long_image = pygame.transform.scale(pygame.image.load('long_platform.png'), (10, 200))
     long_image2 = pygame.transform.flip(long_image, True, False)
     start_image = pygame.transform.scale(pygame.image.load('start.block.png'), (10, 130))
+    teeth_image_r = pygame.transform.scale(pygame.image.load('зуб.png'), (10, 130))
+    teeth_image_l = pygame.transform.flip(teeth_image_r, True, False)
 
     def __init__(self, group):
         super().__init__(group)
         global last_block
         global count_platforms
         global axis
+        global LEVEL
+        global block_w_teeth
+        count_platforms += 1
         self.a = random.randint(1, 2)
-        if count_platforms == 8 and last_block.rect.x < axis and axis == 250:  # сдвиг влево
+        if count_platforms == 9 and last_block.rect.x < axis and axis == 250:  # сдвиг влево
             axis = 190
-        elif count_platforms == 8 and last_block.rect.x > axis and axis == 250:
+        elif count_platforms == 9 and last_block.rect.x > axis and axis == 250:
             axis = 310
-        elif count_platforms == 14:
+        elif count_platforms == 15:
             axis = 250
         self.start = False
+        self.teeth = False
         self.get_image()
         if count_platforms >= 17:
             count_platforms = 1
+            LEVEL += 1
+            block_w_teeth = random.randrange(4, 17)
+            while block_w_teeth == 8 or block_w_teeth == 9 or block_w_teeth == 14 or block_w_teeth == 15:
+                block_w_teeth = random.randrange(4, 16)
             self.image = Block.start_image
             self.block_length = platform_length
             self.rect = self.image.get_rect()
             self.start = True
+        if LEVEL > 1:
+            if count_platforms == block_w_teeth:
+                if (last_block.rect.x < axis and (axis == 250 or axis == 190)) or (
+                        last_block.rect.x <= axis and axis == 310):
+                    self.image = Block.teeth_image_l
+                else:
+                    self.image = Block.teeth_image_r
+                self.block_length = platform_length
+                self.rect = self.image.get_rect()
+                self.teeth = True
+        # if count_platforms == 3:
+        #     self.image = Block.teeth_image
+        #     self.block_length = platform_length
+        #     self.rect = self.image.get_rect()
+        #     self.teeth = True
         self.coord()
         self.replace = False
         self.curr = False
         self.turn = False
-        if count_platforms == 7 or count_platforms == 13:
+        if count_platforms == 8 or count_platforms == 14:
             self.turn = True
         last_block = self
 
     def coord(self):
         b = 0
-        if count_platforms == 9 or count_platforms == 15:
+        if count_platforms == 10 or count_platforms == 16:
             b = 220
         if last_block:
             if (last_block.rect.x < axis and (axis == 250 or axis == 190)) or (
@@ -136,14 +163,14 @@ class Cat(pygame.sprite.Sprite):
         self.jump_counter = 0
         self.jump_1 = False
         self.jump_2 = False
-        self.jump_height = 100
-        self.jump_width = 100
         self.curr_block = self.current_block()  # спрайт текущего блока
         self.direction = 2  # left
         self.gravity_y = 15
         self.gravity_x = 2
         self.check_2_jump = False
         self.is_slip = False
+        self.is_fall = False
+        self.hurt = False
 
     def check_fallen(self):  # кот коснулся предела окна
         return self.player_x > WIDTH or self.player_x < 0 or self.player_y + 40 > HEIGHT
@@ -168,7 +195,7 @@ class Cat(pygame.sprite.Sprite):
 
     def check_collisions(self):
         for el in all_platforms:
-            if el.rect.colliderect([self.player_x, self.player_y, 40, 40]):
+            if el.rect.colliderect([self.player_x, self.player_y, 40, 40]) and not el.teeth:
                 el.curr = True
                 self.curr_block = el
                 if el.rect.x < self.player_x + 10:
@@ -176,6 +203,20 @@ class Cat(pygame.sprite.Sprite):
                 else:
                     self.player_x = el.rect.x - 40
                 return True
+            elif el.rect.colliderect([self.player_x, self.player_y, 40, 40]) and el.teeth:
+                el.curr = True
+                self.curr_block = el
+                self.hurt = True
+                self.gravity_y = 6
+                self.gravity_x = 1
+
+    def jump_back(self):
+        self.player_y -= self.gravity_y
+        if self.curr_block.rect[0] > self.player_x:
+            self.player_x -= self.gravity_x
+        else:
+            self.player_x += self.gravity_x
+        self.gravity_y += 0.2
 
     def one_click(self):
         if self.jump_counter == 1 and not self.check_2_jump:
@@ -221,7 +262,6 @@ class Cat(pygame.sprite.Sprite):
         for el in all_platforms:
             if el.rect.y + platform_length > HEIGHT and not el.replace:
                 Block(all_platforms)
-                count_platforms += 1
                 el.replace = True
 
     def update_turn(self):
@@ -247,12 +287,6 @@ timer = pygame.time.Clock()
 
 # game variables
 
-platforms = [[120, 460, platform_width, platform_length], [220, 400, platform_width, platform_length],
-             [120, 270, platform_width, platform_length], [220, 200, platform_width, platform_length],
-             [130, 70, platform_width, platform_length], [210, 20, platform_width, platform_length]]
-
-v = 50
-t = 1
 
 # create screen
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -267,7 +301,6 @@ last_block = first_block
 first_block.start = True
 for i in range(5):
     Block(all_platforms)
-count_platforms += 6
 while running:
     timer.tick(fps)
     screen.fill(background)
@@ -276,7 +309,7 @@ while running:
         cat.jump = False
         cat.dead = True
         screen.blit(cat.image_dead, (cat.player_x, cat.player_y))
-    elif cat.jump or cat.current_block() is None:
+    elif cat.jump or cat.current_block() is None or cat.hurt:
         screen.blit(cat.image, (cat.player_x, cat.player_y))
     else:
         cat.curr_block = cat.current_block()
@@ -303,7 +336,7 @@ while running:
             cat.update_turn()
         if event.type == pygame.MOUSEBUTTONDOWN:
             cat.direction = 3 - cat.direction
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and not cat.hurt:
             cat.jump_2 = False
             if not cat.check_fall():
                 cat.jump_1 = True
@@ -316,7 +349,7 @@ while running:
                     cat.check_2_jump = False
             else:
                 cat.jump = False
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:  # правая кнопка мыши
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 3 and not cat.hurt:  # правая кнопка мыши
             cat.jump_1 = False
             if not cat.check_fall():
                 cat.jump_2 = True
@@ -338,8 +371,15 @@ while running:
         cat.jump_1 = False
         cat.jump_2 = False
         cat.is_slip = True
+        cat.is_fall = False
+
+    if cat.hurt and not cat.is_fall and not cat.dead:
+        cat.jump_back()
+        if cat.gravity_y == 10:
+            cat.is_fall = True
 
     if cat.check_fall() and not cat.dead:
+        cat.is_fall = True
         cat.fall()
         cat.jump = False
 
